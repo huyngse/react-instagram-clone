4e48cfeaefbe63467a535804911e257c8861b8f2
huyngse <ngogiahuy775309@gmail.com>
2023-12-25 15:06:04 +0700

    Add Sign In with Google Auth

--- src/components/auth/GoogleAuth.jsx
+++ src/components/auth/GoogleAuth.jsx
@@ -1,11 +1,11 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
-
+import useGoogleAuth from "../../hooks/useGoogleAuth";
 const GoogleAuth = (props) => {
-
+  const { signIn } = useGoogleAuth();
   return (
-    <Flex alignItems={'center'} gap={2} cursor={'pointer'}>
-      <Image src='./assets/google.png' h={5} alt='goole logo' />
-      <Text color={'blue.500'}>F
+    <Flex alignItems={'center'} gap={2} cursor={'pointer'} onClick={signIn}>
+      <Image src='./assets/google.png' h={5} alt='google logo' />
+      <Text color={'blue.500'}>
         {
           props.isSignUp ? "Sign up with Google" : "Log in with Google"
         }
