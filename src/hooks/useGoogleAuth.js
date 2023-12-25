9340c755ef1facb64e1c346bce076540e974d5ed
huyngse <ngogiahuy775309@gmail.com>
2023-12-25 15:45:13 +0700

    Fix sign up and login issue

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -2,7 +2,7 @@ import { auth } from "../firebase/firebase";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import useShowToast from "../hooks/useShowToast";
 import useAuthStore from "../store/authStore";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 const useGoogleAuth = () => {
   const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
@@ -16,19 +16,27 @@ const useGoogleAuth = () => {
         return;
       }
       if (newUser) {
-        const userDocument = {
-          uid: newUser.user.uid,
-          email: newUser.user.email,
-          username: newUser.user.email.split("@")[0],
-          fullName: newUser.user.displayName,
-          bio: "",
-          profilePicURL: newUser.user.photoURL,
-          followers: [],
-          following: [],
-          posts: [],
-          createdAt: Date.now(),
-        };
-        await setDoc(doc(firestore, "users", newUser.user.uid), userDocument);
+        // CHECK IF USER IS EXISTED
+        const docRef = doc(firestore, "users", newUser.user.uid);
+        const docSnap = await getDoc(docRef);
+        let userDocument;
+        if (!docSnap.exists()) {
+          userDocument = {
+            uid: newUser.user.uid,
+            email: newUser.user.email,
+            username: newUser.user.email.split("@")[0],
+            fullName: newUser.user.displayName,
+            bio: "",
+            profilePicURL: newUser.user.photoURL,
+            followers: [],
+            following: [],
+            posts: [],
+            createdAt: Date.now(),
+          };
+          await setDoc(doc(firestore, "users", newUser.user.uid), userDocument);
+        } else {
+          userDocument = docSnap.data();
+        }
         localStorage.setItem("userInfo", JSON.stringify(userDocument));
         loginUser(userDocument);
       }
