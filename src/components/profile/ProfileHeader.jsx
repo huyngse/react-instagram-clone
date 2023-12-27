550196de5d2c86d8f2d775d70924aa69ca164735
huyngse <ngogiahuy775309@gmail.com>
2023-12-27 11:15:22 +0700

    Update follow button and edit proifle button

--- src/components/profile/ProfileHeader.jsx
+++ src/components/profile/ProfileHeader.jsx
@@ -1,8 +1,11 @@
 import { Avatar, AvatarGroup, Button, Flex, Text } from "@chakra-ui/react";
 import useUserProfileStore from "../../store/userProfileStore";
-
+import useAuthStore from "../../store/authStore";
 const ProfileHeader = () => {
     const { userProfile } = useUserProfileStore();
+    const authUser = useAuthStore(state => state.user);
+    const isVisitingOwnProfile = authUser && authUser.username === userProfile.username;
+    const isVisitingOtherProfile = authUser && authUser.username !== userProfile.username;
     return (
         <Flex
             my="50px"
@@ -34,14 +37,30 @@ const ProfileHeader = () => {
                     mb={2}
                 >
                     <Text fontSize="25px">{userProfile.username}</Text>
-                    <Button
-                        size="sm"
-                        bg="white"
-                        color="black"
-                        _hover={{ bg: "lightgray" }}
-                    >
-                        Edit Profile
-                    </Button>
+                    {
+                        isVisitingOwnProfile && (
+                            <Button
+                                size="sm"
+                                bg="white"
+                                color="black"
+                                _hover={{ bg: "lightgray" }}
+                            >
+                                Edit Profile
+                            </Button>
+                        )
+                    }
+                    {
+                        isVisitingOtherProfile && (
+                            <Button
+                                size="sm"
+                                bg="blue.500"
+                                color="white"
+                                _hover={{ bg: "blue.600" }}
+                            >
+                                Follow
+                            </Button>
+                        )
+                    }
                 </Flex>
                 <Flex gap={5}>
                     <div>
