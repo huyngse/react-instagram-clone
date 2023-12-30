ac23a114551fda92b40af3b54931d0c698a46dfb
huyngse <ngogiahuy775309@gmail.com>
2023-12-30 09:33:54 +0700

    Add search user function

--- src/components/SideBar/Search.jsx
+++ src/components/SideBar/Search.jsx
@@ -1,31 +1,89 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Tooltip, useDisclosure } from "@chakra-ui/react";
 import { SearchLogo } from "../../assets/constants";
+import useSearchUser from "../../hooks/useSearchUser";
+import { useRef } from "react";
+import SuggestedUser from "../home/SuggestedUser";
 const Search = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isLoading, getUserProfile, user, setUser } = useSearchUser();
+    const searchRef = useRef(null);
+    const handleSearchUser = (e) => {
+        e.preventDefault();
+        getUserProfile(searchRef.current.value);
+    }
     return (
-        <Tooltip
-            hasArrow
-            label="Search"
-            placement="right"
-            ml={1}
-            openDelay={500}
-            display={{ base: 'block', md: 'none' }}
-        >
-            <Flex
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                display="flex"
-                alignItems="center"
-                justifyContent={{ base: 'center', md: 'flex-start' }}
-                gap={4}
-                w={{ base: 10, md: 'full' }}
+        <>
+            <Tooltip
+                hasArrow
+                label="Search"
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{ base: 'block', md: 'none' }}
             >
-                <SearchLogo />
-                <Box display={{ base: 'none', md: 'block' }}>
-                    Search
-                </Box>
-            </Flex>
-        </Tooltip>
+                <Flex
+                    _hover={{ bg: "whiteAlpha.400" }}
+                    borderRadius={6}
+                    p={2}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent={{ base: 'center', md: 'flex-start' }}
+                    gap={4}
+                    w={{ base: 10, md: 'full' }}
+                    onClick={onOpen}
+                >
+                    <SearchLogo />
+                    <Box display={{ base: 'none', md: 'block' }}>
+                        Search
+                    </Box>
+                </Flex>
+            </Tooltip>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+                motionPreset="slideInLeft"
+                isCentered
+            >
+                <ModalOverlay />
+                <ModalContent bg="black" border="1px solid gray" maxW="400px">
+                    <ModalHeader>
+                        Search user
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <form onSubmit={handleSearchUser}>
+                            <FormControl>
+                                <FormLabel>Username</FormLabel>
+                                <Input placeholder="Bob Robber" ref={searchRef} />
+                            </FormControl>
+                            <Flex
+                                w="full"
+                                justifyContent="flex-end"
+                            >
+                                <Button
+                                    type="submit"
+                                    ml="auto"
+                                    size="sm"
+                                    my={4}
+                                    isLoading={isLoading}
+                                >
+                                    Search
+                                </Button>
+                            </Flex>
+                        </form>
+                        {
+                            user && (
+                                <SuggestedUser
+                                    user={user}
+                                    setUser={setUser}
+                                />
+                            )
+                        }
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+
     )
 }
 
---
+++ src/hooks/useSearchUser.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import useShowToast from "./useShowToast";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+const useSearchUser = () => {
+  const [isLoading, SetIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const showToast = useShowToast();
+  const getUserProfile = async (username) => {
+    SetIsLoading(true);
+    try {
+      const q = query(
+        collection(firestore, "users"),
+        where("username", "==", username)
+      );
+      const querySnap = await getDocs(q);
+      if (querySnap.empty) {
+        showToast("Error", "User not found", "error");
+        return;
+      }
+      querySnap.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      showToast("Error", error.message, "error");
+      setUser(null);
+    } finally {
+      SetIsLoading(false);
+    }
+  };
+  return { isLoading, getUserProfile, user, setUser };
+};
+
+export default useSearchUser;
